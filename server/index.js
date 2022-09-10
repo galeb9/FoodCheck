1e61f0a11fb0c3de2394a3ec57c5925e72e7f649
Matej Grimšič <matej.grimsic@hudlajf.com>
2022-09-10 13:46:28 +0200

    Feat: server files added

--- server/index.js
+++ server/index.js
@@ -12,6 +12,14 @@ app.use(cors());
 const list = require('./routes/api/list');
 app.use('/api/list/', list)
 
+// Handle production
+if(process.env.NODE_ENV === 'production') {
+    // static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 // connect to mongoose
 const port = process.env.port || 5000;
@@ -19,7 +27,7 @@ const dbURI = "mongodb+srv://galeb9:ZnanjeJeMoc6@foodcheck.zbjtfcd.mongodb.net/?
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => {
-        app.listen(port, () => console.log(`It is alive on the river banks of Rohan: http://localhost:${port}`))
+        app.listen(port, () => console.log(`Server is alive on the river banks of Rohan: http://localhost:${port}/api/list`))
         console.log("connected to db")
     } )
     .catch(err => console.log(err))
